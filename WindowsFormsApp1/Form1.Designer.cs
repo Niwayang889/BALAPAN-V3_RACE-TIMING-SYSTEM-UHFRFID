25b1afe0db740d5f2b1a4842e7c6d23dab17501b
putra <putra@DESKTOP-1NJL6AG>
2024-01-11 01:30:51 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.CONNECT = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBoxlog = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_rs232 = new System.Windows.Forms.RadioButton();
+            this.gpb_rs232 = new System.Windows.Forms.GroupBox();
+            this.btDisConnect232 = new System.Windows.Forms.Button();
+            this.btConnect232 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ComboBox_baud2 = new System.Windows.Forms.ComboBox();
+            this.ComboBox_COM = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.CONNECT.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.gpb_rs232.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.CONNECT);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1141, 435);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // CONNECT
+            // 
+            this.CONNECT.Controls.Add(this.richTextBoxlog);
+            this.CONNECT.Controls.Add(this.groupBox1);
+            this.CONNECT.Controls.Add(this.gpb_rs232);
+            this.CONNECT.Location = new System.Drawing.Point(4, 25);
+            this.CONNECT.Name = "CONNECT";
+            this.CONNECT.Padding = new System.Windows.Forms.Padding(3);
+            this.CONNECT.Size = new System.Drawing.Size(1133, 406);
+            this.CONNECT.TabIndex = 0;
+            this.CONNECT.Text = "CONNECT";
+            this.CONNECT.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1133, 406);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBoxlog
+            // 
+            this.richTextBoxlog.Location = new System.Drawing.Point(574, 6);
+            this.richTextBoxlog.Name = "richTextBoxlog";
+            this.richTextBoxlog.Size = new System.Drawing.Size(477, 145);
+            this.richTextBoxlog.TabIndex = 9;
+            this.richTextBoxlog.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb_rs232);
+            this.groupBox1.Location = new System.Drawing.Point(7, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(457, 59);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connect Type";
+            // 
+            // rb_rs232
+            // 
+            this.rb_rs232.AutoSize = true;
+            this.rb_rs232.Location = new System.Drawing.Point(44, 25);
+            this.rb_rs232.Margin = new System.Windows.Forms.Padding(4);
+            this.rb_rs232.Name = "rb_rs232";
+            this.rb_rs232.Size = new System.Drawing.Size(68, 20);
+            this.rb_rs232.TabIndex = 0;
+            this.rb_rs232.Text = "RS232";
+            this.rb_rs232.UseVisualStyleBackColor = true;
+            // 
+            // gpb_rs232
+            // 
+            this.gpb_rs232.Controls.Add(this.btDisConnect232);
+            this.gpb_rs232.Controls.Add(this.btConnect232);
+            this.gpb_rs232.Controls.Add(this.label2);
+            this.gpb_rs232.Controls.Add(this.ComboBox_baud2);
+            this.gpb_rs232.Controls.Add(this.ComboBox_COM);
+            this.gpb_rs232.Controls.Add(this.label1);
+            this.gpb_rs232.Location = new System.Drawing.Point(7, 78);
+            this.gpb_rs232.Margin = new System.Windows.Forms.Padding(4);
+            this.gpb_rs232.Name = "gpb_rs232";
+            this.gpb_rs232.Padding = new System.Windows.Forms.Padding(4);
+            this.gpb_rs232.Size = new System.Drawing.Size(457, 94);
+            this.gpb_rs232.TabIndex = 8;
+            this.gpb_rs232.TabStop = false;
+            this.gpb_rs232.Text = "RS232";
+            // 
+            // btDisConnect232
+            // 
+            this.btDisConnect232.Enabled = false;
+            this.btDisConnect232.Location = new System.Drawing.Point(303, 54);
+            this.btDisConnect232.Margin = new System.Windows.Forms.Padding(4);
+            this.btDisConnect232.Name = "btDisConnect232";
+            this.btDisConnect232.Size = new System.Drawing.Size(120, 31);
+            this.btDisConnect232.TabIndex = 20;
+            this.btDisConnect232.Text = "Disconnect";
+            this.btDisConnect232.UseVisualStyleBackColor = true;
+            // 
+            // btConnect232
+            // 
+            this.btConnect232.Location = new System.Drawing.Point(303, 16);
+            this.btConnect232.Margin = new System.Windows.Forms.Padding(4);
+            this.btConnect232.Name = "btConnect232";
+            this.btConnect232.Size = new System.Drawing.Size(120, 31);
+            this.btConnect232.TabIndex = 19;
+            this.btConnect232.Text = "Connect";
+            this.btConnect232.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Baud rate:";
+            // 
+            // ComboBox_baud2
+            // 
+            this.ComboBox_baud2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox_baud2.FormattingEnabled = true;
+            this.ComboBox_baud2.Items.AddRange(new object[] {
+            "9600bps",
+            "19200bps",
+            "38400bps",
+            "57600bps",
+            "115200bps"});
+            this.ComboBox_baud2.Location = new System.Drawing.Point(107, 55);
+            this.ComboBox_baud2.Margin = new System.Windows.Forms.Padding(4);
+            this.ComboBox_baud2.Name = "ComboBox_baud2";
+            this.ComboBox_baud2.Size = new System.Drawing.Size(160, 24);
+            this.ComboBox_baud2.TabIndex = 18;
+            // 
+            // ComboBox_COM
+            // 
+            this.ComboBox_COM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox_COM.FormattingEnabled = true;
+            this.ComboBox_COM.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9"});
+            this.ComboBox_COM.Location = new System.Drawing.Point(107, 21);
+            this.ComboBox_COM.Margin = new System.Windows.Forms.Padding(4);
+            this.ComboBox_COM.Name = "ComboBox_COM";
+            this.ComboBox_COM.Size = new System.Drawing.Size(160, 24);
+            this.ComboBox_COM.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Serial Port:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1165, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BALAPANV3";
+            this.Load += new System.EventHandler(this.BALAPANV3_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.CONNECT.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.gpb_rs232.ResumeLayout(false);
+            this.gpb_rs232.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage CONNECT;
+        private System.Windows.Forms.RichTextBox richTextBoxlog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb_rs232;
+        private System.Windows.Forms.GroupBox gpb_rs232;
+        private System.Windows.Forms.Button btDisConnect232;
+        private System.Windows.Forms.Button btConnect232;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox ComboBox_baud2;
+        internal System.Windows.Forms.ComboBox ComboBox_COM;
+        internal System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UHF;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private byte fBaud;
+        public static byte fComAdr = 0xff;
+        private int fCmdRet = 30;
+        public static int frmcomportindex;
+
+        RFIDCallBack elegateRFIDCallBack;
+
+        RfidTagCallBack myCallBack;
+
+        #region 
+        private string GetReturnCodeDesc(int cmdRet)
+        {
+            switch (cmdRet)
+            {
+                case 0x00:
+                case 0x26:
+                    return "success";
+                case 0x01:
+                    return "Return before Inventory finished";
+                case 0x02:
+                    return "the Inventory-scan-time overflow";
+                case 0x03:
+                    return "More Data";
+                case 0x04:
+                    return "Reader module MCU is Full";
+                case 0x05:
+                    return "Access Password Error";
+                case 0x09:
+                    return "Destroy Password Error";
+                case 0x0a:
+                    return "Destroy Password Error Cannot be Zero";
+                case 0x0b:
+                    return "Tag Not Support the command";
+                case 0x0c:
+                    return "Use the commmand,Access Password Cannot be Zero";
+                case 0x0d:
+                    return "Tag is protected,cannot set it again";
+                case 0x0e:
+                    return "Tag is unprotected,no need to reset it";
+                case 0x10:
+                    return "There is some locked bytes,write fail";
+                case 0x11:
+                    return "can not lock it";
+                case 0x12:
+                    return "is locked,cannot lock it again";
+                case 0x13:
+                    return "Parameter Save Fail,Can Use Before Power";
+                case 0x14:
+                    return "Cannot adjust";
+                case 0x15:
+                    return "Return before Inventory finished";
+                case 0x16:
+                    return "Inventory-Scan-Time overflow";
+                case 0x17:
+                    return "More Data";
+                case 0x18:
+                    return "Reader module MCU is full";
+                case 0x19:
+                    return "'Not Support Command Or AccessPassword Cannot be Zero";
+                case 0x1A:
+                    return "Tag custom function error";
+                case 0xF8:
+                    return "Check antenna error";
+                case 0xF9:
+                    return "Command execute error";
+                case 0xFA:
+                    return "Get Tag,Poor Communication,Inoperable";
+                case 0xFB:
+                    return "No Tag Operable";
+                case 0xFC:
+                    return "Tag Return ErrorCode";
+                case 0xFD:
+                    return "Command length wrong";
+                case 0xFE:
+                    return "Illegal command";
+                case 0xFF:
+                    return "Parameter Error";
+                case 0x30:
+                    return "Communication error";
+                case 0x31:
+                    return "CRC checksummat error";
+                case 0x32:
+                    return "Return data length error";
+                case 0x33:
+                    return "Communication busy";
+                case 0x34:
+                    return "Busy,command is being executed";
+                case 0x35:
+                    return "ComPort Opened";
+                case 0x36:
+                    return "ComPort Closed";
+                case 0x37:
+                    return "Invalid Handle";
+                case 0x38:
+                    return "Invalid Port";
+                case 0xEE:
+                    return "Return Command Error";
+                default:
+                    return Convert.ToString(cmdRet, 16);
+            }
+        }
+        private string GetErrorCodeDesc(int cmdRet)
+        {
+            switch (cmdRet)
+            {
+                case 0x00:
+                    return "Other error";
+                case 0x03:
+                    return "Memory out or pc not support";
+                case 0x04:
+                    return "Memory Locked and unwritable";
+                case 0x0b:
+                    return "No Power,memory write operation cannot be executed";
+                case 0x0f:
+                    return "Not Special Error,tag not support special errorcode";
+                default:
+                    return "";
+            }
+        }
+        #endregion
+
+        private void rb_rs232_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb_rs232.Checked)
+                if (frmcomportindex > 0 && frmcomportindex < 256)
+                {
+                    if (frmcomportindex > 0)
+                        fCmdRet = RWDev.CloseNetPort(frmcomportindex);
+                    if (fCmdRet == 0)
+                    {
+                        frmcomportindex = -1;
+                        gpb_rs232.Enabled = true;
+                        btDisConnect232.Enabled = false;
+                        // Set button font color
+                        btConnect232.ForeColor = Color.Indigo;
+                        SetButtonBold(btConnect232);
+                    }
+                }
+        }
+
+        private void btConnect232_Click(object sender, EventArgs e)
+        {
+            int portNum = ComboBox_COM.SelectedIndex + 1;
+            int FrmPortIndex = 0;
+            string strException = string.Empty;
+            fBaud = Convert.ToByte(ComboBox_baud2.SelectedIndex);
+            if (fBaud > 2)
+                fBaud = Convert.ToByte(fBaud + 2);
+            fComAdr = 255;//广播地址打开设备
+            fCmdRet = RWDev.OpenComPort(portNum, ref fComAdr, fBaud, ref FrmPortIndex);
+            if (fCmdRet != 0)
+            {
+                string strLog = "Connect reader failed: " + GetReturnCodeDesc(fCmdRet);
+                richTextBoxlog.AppendText(strLog + "\n"); // Display error in richTextBoxlog
+                return;
+            }
+            else
+            {
+                frmcomportindex = FrmPortIndex;
+                string strLog = "Connected: " + ComboBox_COM.Text + "@" + ComboBox_baud2.Text;
+                richTextBoxlog.AppendText(strLog + "\n"); // Display success in richTextBoxlog
+            }
+
+            //处理界面元素是否有效
+            //EnabledForm();
+            btConnect232.Enabled = false;
+            btDisConnect232.Enabled = true;
+            //设置按钮字体颜色
+            btConnect232.ForeColor = Color.Black;
+            btDisConnect232.ForeColor = Color.Indigo;
+            SetButtonBold(btConnect232);
+            SetButtonBold(btDisConnect232);
+            //btGetInformation_Click(null, null);//获取读写器信息
+            if (FrmPortIndex > 0)
+                RWDev.InitRFIDCallBack(elegateRFIDCallBack, true, FrmPortIndex);
+        }
+
+
+        private void btDisConnect232_Click(object sender, EventArgs e)
+        {
+            if (frmcomportindex > 0)
+                fCmdRet = RWDev.CloseSpecComPort(frmcomportindex);
+            if (fCmdRet == 0)
+            {
+                frmcomportindex = -1;
+                // Update button states and colors
+                btConnect232.Enabled = true;
+                btDisConnect232.Enabled = false;
+                btConnect232.ForeColor = Color.Indigo;
+                btDisConnect232.ForeColor = Color.Black;
+                SetButtonBold(btConnect232);
+                SetButtonBold(btDisConnect232);
+                richTextBoxlog.AppendText("COM close success\n"); // Log the success message
+            }
+            else
+            {
+                string strLog = "COM close failed: " + GetReturnCodeDesc(fCmdRet);
+                richTextBoxlog.AppendText(strLog + "\n"); // Log the failure message
+            }
+        }
+
+        private void BALAPANV3_Load(object sender, EventArgs e)
+        {
+            ComboBox_COM.SelectedIndex = 0; // Misalnya, memilih COM1 sebagai default
+            ComboBox_baud2.SelectedIndex = 3;
+        }
+
+        private void SetButtonBold(Button btnBold)
+        {
+            Font oldFont = btnBold.Font;
+            Font newFont = new Font(oldFont, oldFont.Style ^ FontStyle.Bold);
+            btnBold.Font = newFont;
+        }
+    }
+}
